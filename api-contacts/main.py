d6c7ca2e8404505cb438051d49eeb8f8198ec5ab
dev-diegov <diegoidv.us@gmail.com>
2023-11-23 20:50:15 -0300

    calling routes and register blueprints

--- api-contacts/main.py
+++ api-contacts/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
+from routes.userRoutes import user_api
+from routes.contactRoutes import contact_api
 
 # defining a flask application
 app = Flask(__name__)
 
+app.register_blueprint(user_api)
+app.register_blueprint(contact_api)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-    if app.run == True:
-        print('app running')
