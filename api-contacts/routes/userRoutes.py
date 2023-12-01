78dc336c8bb9231b439a16c0d0e63ac8a7234adb
dev-diegov <devdiegodv@gmail.com>
2023-11-30 21:32:47 -0300

    testing user connection

--- api-contacts/routes/userRoutes.py
+++ api-contacts/routes/userRoutes.py
@@ -1,7 +1,17 @@
 from flask import Flask, jsonify, request, Blueprint
+from models import User
+from db import connect
 
 user_api = Blueprint('user_api', __name__)
 
 @user_api.route('/user', methods= ['GET'])
 def user():
-    return 'Blueprint working'
+    try:
+        session = connect()
+        users = session.query(User).all()
+        print(users)
+    except:
+        print('The user could not connect to the database')
+    finally:
+        session.close()
+        
