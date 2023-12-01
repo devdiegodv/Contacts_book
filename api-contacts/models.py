dbdbb865d721b45ec54f81b51355064afac5f7fd
dev-diegov <devdiegodv@gmail.com>
2023-11-30 21:30:35 -0300

    creation of tables

--- api-contacts/models.py
+++ api-contacts/models.py
@@ -1,6 +1,13 @@
-import datetime
+from datetime import datetime
+from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
-class User():
+
+Base = declarative_base()
+
+@dataclass
+class User(Base):
     """
     A class representing a user with basic information.
 
@@ -20,6 +27,13 @@ class User():
     email: str
     password: str
 
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+
     def __repr__(self) -> str:
         """
         Returns a string representation of the User object.
@@ -27,9 +41,10 @@ class User():
         Returns:
         str: A formatted string containing user information.
         """
-        return f'<User(id={self.id}, name={self.name}, last_name={self.last_name}, email={self.email}, password={self.password})'
+        return f'<User(id={self.id}, name={self.name}, last_name={self.last_name}, email={self.email}, password={self.password})>'
 
-class Contact():
+@dataclass
+class Contact(Base):
     """
     A class representing a contact with basic information.
 
@@ -49,3 +64,42 @@ class Contact():
     email: str
     phone_number: str
     creation_date: datetime
+
+    today_date = datetime.today()
+
+    __tablename__ = 'contact'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    phone_number = Column(String, nullable=False)
+    creation_date = Column(DateTime, default=datetime.today().date())
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the Contact object.
+
+        Returns:
+        str: A formatted string containing contact information.
+        """
+        return f'<Contact(id={self.id}, name={self.name}, last_name={self.last_name}, address={self.address}, email={self.email}, phone_number={self.phone_number}, creation_date={self.creation_date})>'
+
+@dataclass    
+class BelongsTo(Base):
+    """
+    A class representing the association between users and contacts.
+
+    Attributes:
+    - id (int): BelongsTo's unique identifier.
+    - user_id (int): User's unique identifier.
+    - contact_id (int): Contact's unique identifier.
+    """
+    id: int
+    user_id: int
+    contact_id: int
+
+    __tablename__ = 'belongsto'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    contact_id = Column(Integer, nullable=False)
