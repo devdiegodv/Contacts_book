0fc883ba6705254906fb18bda6e245f17ee85257
dev-diegov <devdiegodv@gmail.com>
2023-12-06 22:36:14 -0300

    selectContacts function for endpoint

---
+++ api-contacts/controllers/contactController.py
@@ -0,0 +1,32 @@
+from db import connect
+from models import Contact, BelongsTo
+
+def selectContacts(user_id, field, order):
+    """
+    Retrieve a list of contacts for a given user, sorted based on specified field and order.
+
+    Parameters:
+    - user_id (int): The unique identifier for the user.
+    - field (str): The field by which contacts should be sorted ('ID' or 'NAME').
+    - order (str): The order in which contacts should be sorted ('ASC' for ascending, 'DESC' for descending).
+
+    Returns:
+    - contacts (list): A list of Contact objects sorted according to the specified field and order.
+    """
+    try:
+        session = connect()
+
+        if field == 'ID' and order == 'ASC':
+            contacts = session.query(Contact).join(BelongsTo, Contact.id == BelongsTo.contact_id).filter(BelongsTo.user_app_id == user_id).order_by(Contact.id).all()
+        elif field == 'ID' and order == 'DESC':
+            contacts = session.query(Contact).join(BelongsTo, Contact.id == BelongsTo.contact_id).filter(BelongsTo.user_app_id == user_id).order_by(Contact.id.desc()).all()
+        elif field == 'NAME' and order == 'ASC':
+            contacts = session.query(Contact).join(BelongsTo, Contact.id == BelongsTo.contact_id).filter(BelongsTo.user_app_id == user_id).order_by(Contact.name).all()
+        elif field == 'NAME' and order == 'DESC':
+            contacts = session.query(Contact).join(BelongsTo, Contact.id == BelongsTo.contact_id).filter(BelongsTo.user_app_id == user_id).order_by(Contact.name.desc()).all()
+    except Exception as e:
+        print(e)
+    finally:
+        session.close()
+
+    return contacts
