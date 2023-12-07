a420e4a4b6b108107d2be7e05027781d55951519
dev-diegov <devdiegodv@gmail.com>
2023-12-06 21:53:53 -0300

    endpoint GET for login

--- api-contacts/routes/userRoutes.py
+++ api-contacts/routes/userRoutes.py
@@ -2,19 +2,16 @@ from flask import Flask, jsonify, request, Blueprint
 from models import User
 from db import connect
 
+import controllers.userController as userController
+
 user_api = Blueprint('user_api', __name__)
 
 @user_api.route('/user', methods= ['GET'])
-def user():
-    try:
-        session = connect()
-        users = session.query(User).all()
-        print(users)
-    except Exception as e:
-        print('The user could not connect to the database')
-        print(e)
-    finally:
-        session.close()
-    
-    return 'Query done'
-        
+def getUser():
+    parameters = request.args
+    print("esto es request data -> ", request.data)
+    print("esto es parameters -> ", parameters)
+    email = parameters['email']
+    password = parameters['password']
+    result = userController.selectUser(email, password)
+    return jsonify({'result': result})
