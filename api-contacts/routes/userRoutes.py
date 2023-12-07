6dde71a8b249eeabd97d821d86d88f4725e112e5
dev-diegov <devdiegodv@gmail.com>
2023-12-06 22:33:11 -0300

    getUser documented and prints deleted

--- api-contacts/routes/userRoutes.py
+++ api-contacts/routes/userRoutes.py
@@ -8,9 +8,27 @@ user_api = Blueprint('user_api', __name__)
 
 @user_api.route('/user', methods= ['GET'])
 def getUser():
+    """
+    Endpoint to retrieve user information based on provided email and password.
+
+    HTTP Method:
+    - GET
+
+    Request Parameters:
+    - email (str): The email address of the user.
+    - password (str): The password associated with the user's account.
+
+    Returns:
+    - JSON response containing the result of the user retrieval operation.
+      The result includes the user information if found, or a default result if not found.
+
+    Example Usage:
+    - GET /user?email=user@example.com&password=secretpassword
+
+    Example Response:
+    - {'result': 1}  # Where 1 is the user ID, or 0 if the user is not found.
+    """
     parameters = request.args
-    print("esto es request data -> ", request.data)
-    print("esto es parameters -> ", parameters)
     email = parameters['email']
     password = parameters['password']
     result = userController.selectUser(email, password)
