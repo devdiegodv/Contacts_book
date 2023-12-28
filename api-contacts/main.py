02f60200e3f2ad331d8df30a5a7e092d815f8345
dev-diegov <devdiegodv@gmail.com>
2023-12-28 10:28:50 -0300

    CORS fix

--- api-contacts/main.py
+++ api-contacts/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from routes.userRoutes import user_api
 from routes.contactRoutes import contact_api
 
 # defining a flask application
 app = Flask(__name__)
+CORS(app) # avoid CORS problem
 
 app.register_blueprint(user_api)
 app.register_blueprint(contact_api)
