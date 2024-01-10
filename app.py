e9af4bb26267ea6f7033b0cb1315061710873699
dev-diegov <devdiegodv@gmail.com>
2024-01-09 23:17:09 -0300

    changed debug True to False

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ app.register_blueprint(user_api)
 app.register_blueprint(contact_api)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
